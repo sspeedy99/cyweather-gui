038ec773d34707551f025ce677b53b87637793e6
Shashi Prakash <sspeedysingh096@gmail.com>
2019-11-20 22:27:30 +0530

    Integrate the City class

--- src/cyweather/Weather.java
+++ src/cyweather/Weather.java
@@ -1,11 +1,23 @@
 package cyweather;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Weather {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Hello");
-
+	public static void main(String [] args) {
+		//System.out.println("hello");
+		City city = new City(22,21,"Hubli");
+		//city.setCity();
+		try {
+		city.showNow();
+		System.out.println("Enter the city to find its weather");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		city.getCity(input);
 	}
-
+		catch (IOException e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+	}
 }
+
